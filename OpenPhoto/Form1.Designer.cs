4d5aad00171032ed80d947d8161c3345e811350b
AILOVEU <1032290763@qq.com>
2020-02-27 12:24:14 +0800

    init

---
+++ OpenPhoto/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace OpenPhoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.showPicBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.showPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showPicBox
+            // 
+            this.showPicBox.Location = new System.Drawing.Point(-2, 112);
+            this.showPicBox.Name = "showPicBox";
+            this.showPicBox.Size = new System.Drawing.Size(707, 449);
+            this.showPicBox.TabIndex = 0;
+            this.showPicBox.TabStop = false;
+            this.showPicBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(245, 40);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(200, 30);
+            this.openBtn.TabIndex = 1;
+            this.openBtn.Text = "打开图片";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 561);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.showPicBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "打开图片";
+            ((System.ComponentModel.ISupportInitialize)(this.showPicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox showPicBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openBtn;
+    }
+}
+
---
+++ OpenPhoto/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OpenPhoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        byte[] pictureBytes;
+        //定义按钮点击事件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "*.jpg|*.JPG|*.gif|*.GIF|*.bmp|*.BMP";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fullPath = openFileDialog1.FileName;
+                FileStream fs = new FileStream(fullPath, FileMode.Open);
+                pictureBytes = new byte[fs.Length];
+                BinaryReader br = new BinaryReader(fs);
+                pictureBytes = br.ReadBytes(Convert.ToInt32(fs.Length));
+                MemoryStream ms = new MemoryStream(pictureBytes);
+                Bitmap bmpt = new Bitmap(ms);
+                //显示图片
+                showPicBox.Image = bmpt;
+            }
+            else
+            {
+                MessageBox.Show("图片打开失败");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
